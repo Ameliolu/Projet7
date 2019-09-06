563cd0f4714b0b524e4a4aa30c56241d334b01d5
LuK <lsniezak@gmail.com>
2019-09-06 22:27:36 +0200

    correction bouton entrée

--- papybot/static/cours2.js
+++ papybot/static/cours2.js
@@ -2,44 +2,11 @@ var questionElt = document.getElementById("question");
 
 // Pk?
 
-// $(document).ready(function(){
+$(document).keyup(function(touche){
 
-    // $("button").click(function(){
-
-        // $.ajax({
-            // url : '/',
-            // type : 'POST',
-            // data : questionElt.value,
-
-            // success : function(resp){
-                // if (resp['histoire'] != 'vide') {
-                    // $('p[class="dialogue"]:last').append('<p class="dialogue">' + resp['histoire'] + '</p>');
-                // }
-                // else {
-                    // $('p[class="dialogue"]:last').append('<p class="dialogue">Ma mémoire doit me jouer des tours, attends...</p>');
-                // }
-                // if (resp['adresse'] != 'vide') {
-                    // $('p[class="dialogue"]:last').append('<p class="dialogue">Ce lieu est situé ' + resp['adresse'] + '</p>');
-                    // $('p[class="dialogue"]:last').append('<p class="dialogue"><iframe src=' + resp['url_carte'] + '></iframe></p>');
-                // }
-                // else {
-                    // $('p[class="dialogue"]:last').append('<p class="dialogue">J\'ai beau réfléchir...</p>');
-                // }
-
-                // $('p[class="dialogue"]:last').append('<p class="dialogue">Une autre question mon enfant ?</p>');
-            // }
-            
-            
-        // });
-
-    // });
-
-// });
-
-$(document).ready(function(){
-
-    $("button").click(function(){
-
+    var appui = touche.which || touche.keyCode;
+    if(appui == 13){
+        
         $('p[class="dialogue"]:last').append('<img src="http://www.mediaforma.com/sdz/jquery/ajax-loader.gif" class="chargement">');
 
         $.ajax({
@@ -67,8 +34,45 @@ $(document).ready(function(){
                 $('p[class="dialogue"]:last').append('<p class="dialogue">Une autre question mon enfant ?</p>');
                 $('p[class="dialogue"]:last').append('<br />');
             }
-            
-            
+        
+        });
+
+    }
+
+});
+
+$(document).ready(function(){
+
+    $("button").click(function(){
+        
+        $('p[class="dialogue"]:last').append('<img src="http://www.mediaforma.com/sdz/jquery/ajax-loader.gif" class="chargement">');
+
+        $.ajax({
+            url : '/',
+            type : 'POST',
+            data : questionElt.value,
+
+            success : function(resp){
+                $('img[class="chargement"]').remove();
+                
+                if (resp['histoire'] != 'vide') {
+                    $('p[class="dialogue"]:last').append('<p class="dialogue">' + resp['histoire'] + '</p>');
+                }
+                else {
+                    $('p[class="dialogue"]:last').append('<p class="dialogue">Ma mémoire doit me jouer des tours, attends...</p>');
+                }
+                if (resp['adresse'] != 'vide') {
+                    $('p[class="dialogue"]:last').append('<p class="dialogue">Ce lieu est situé ' + resp['adresse'] + '</p>');
+                    $('p[class="dialogue"]:last').append('<p class="dialogue"><iframe src=' + resp['url_carte'] + '></iframe></p>');
+                }
+                else {
+                    $('p[class="dialogue"]:last').append('<p class="dialogue">J\'ai beau réfléchir...</p>');
+                }
+
+                $('p[class="dialogue"]:last').append('<p class="dialogue">Une autre question mon enfant ?</p>');
+                $('p[class="dialogue"]:last').append('<br />');
+            }
+        
         });
 
     });
