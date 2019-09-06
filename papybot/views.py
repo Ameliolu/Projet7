e5d229b8c473dba2c8082f4a713dd6b5b63f6e57
LuK <lsniezak@gmail.com>
2019-09-06 21:32:54 +0200

    correction 4

--- papybot/views.py
+++ papybot/views.py
@@ -18,7 +18,7 @@ app.config.from_object('config')
 
 GOOGLE_KEY = app.config['GO_KEY']
 
-STOP_WORDS = ['le','la','les', 'Salut', 'GrandPy', '!', '.', 'Est-ce', 'que', 'tu', 'connais', 'l\'adresse', 'd\'', '?']
+STOP_WORDS = ['le','la','les', 'Salut', 'GrandPy', '!', '.', 'Est-ce', 'que', 'tu', 'connais', 'l\'adresse', 'd\'', '?', ' ']
  
 @app.route('/', methods=['GET', 'POST'])
 def contact():
